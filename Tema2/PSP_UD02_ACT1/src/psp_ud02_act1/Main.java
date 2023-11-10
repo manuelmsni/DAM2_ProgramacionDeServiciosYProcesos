fb04de8426a97fd2615e96834254dd5b22c6ae7a
manuelmsni <manuelmsni@hotmail.com>
2023-11-10 20:17:55 +0100

    pueba

---
+++ Tema2/PSP_UD02_ACT1/src/psp_ud02_act1/Main.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package psp_ud02_act1;
+
+import psp_ud02_act1.actividad1.ej1_GUI;
+
+/**
+ *
+ * @author Vespertino
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ej1_GUI gui1 = new ej1_GUI();
+        gui1.show();
+    }
+    
+}
